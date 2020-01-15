edaf6493934c084b1269bfe8968b5b0eaf908849
X_zzzzz <195286381@qq.com>
2020-01-15 11:01:55 +0800

    Update webgis_server_restart.sh

--- webgis_server_restart.sh
+++ webgis_server_restart.sh
@@ -13,19 +13,23 @@ webgis_server_path=/data2/zzw/jetty/jetty-distribution-9.4.19.v20190610/
 # 重启webgis服务.
 function webgis_server_restart() {
 	echo '===== 关闭WebGis服务器 ====='
-	echo "ps aux | grep -v grep | grep $webgis_server_container_type | grep $webgis_server_path | awk '{print $2}' | xargs kill -9"
-	ps aux | grep -v grep | grep $server_container_type | grep $webgis_server_path | awk '{print $2}' | xargs kill -9
+	echo "执行命令: ps aux | grep -v grep | grep $webgis_server_container_type | grep $webgis_server_path | awk '{print \$2}' | xargs kill -9"
+	ps aux | grep -v grep | grep $webgis_server_container_type | grep $webgis_server_path | awk '{print $2}' | xargs kill -9
 	echo '===== 重启WebGis服务器 ====='
 	if [[ $webgis_server_container_type == "tomcat" ]]; then
 		bash $webgis_server_path/bin/startup.sh
+		echo "执行命令: bash $webgis_server_path/bin/startup.sh"
 	elif [[ $webgis_server_container_type == "jetty" ]]; then
+		echo "执行命令: bash $webgis_server_path/bin/jetty.sh restart"
 		bash $webgis_server_path/bin/jetty.sh restart
 	else
-		echo '$webgis_server_container_type 类型未知!, 重启服务器失败'
+		echo '服务器类型类型未知! 请检查 webgis_server_container_type 配置项!'
 	fi
 }
 
 
 # main
-echo '调用webgis_server_restart脚本!'
+echo '============================='
+echo '调用webgis_server_restart脚本'
+echo '============================='
 webgis_server_restart
